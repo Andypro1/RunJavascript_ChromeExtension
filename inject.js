ad3d72a4b0608165d9c0c3bb0810530a4510f865
Andypro1 <871829+Andypro1@users.noreply.github.com>
2021-03-31 20:25:39 -0400

    Cleaned up all the unused code, used addListener() in background.js per recommended practices, and fixed the chrome message error - the inject.js script was unnecessarily passing a message to nowhere.

--- inject.js
+++ inject.js
@@ -1,20 +1,17 @@
-chrome.extension.sendMessage({}, function(response) {
-    var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
-            chrome.storage.sync.get(function(obj) {
-                var js = obj['runjavascript_'+location.host];
-                if (typeof js == undefined) {
-                    js = {'code':'','enabled':'true','library':'jquery_3_3_1'};
-                } else if (typeof js == 'string') {
-                    js = {'code':js,'enabled':'true','library':'jquery_3_3_1'};
-                }
-                if (js && js.enabled) {
-                    if (js.code && js.code!="") {
-                        eval(js.code);
-                    }
-                }
-            });
-        }
-    }, 10);
-});
+const runStoredJs = function(e) {
+    chrome.storage.sync.get((obj) => {
+        const js = obj[`runjavascript_${location.host}`];
+
+         if(js)
+            eval(js.code);
+    });
+};
+
+//  Wait until the document is complete before running any scripts for this page
+if(document.readyState === 'complete')
+    runStoredJs();
+else
+    document.addEventListener('readystatechange', e => {
+        if(e.target.readyState === 'complete')
+            runStoredJs();
+    });
--- popup.js
+++ popup.js
@@ -5,46 +5,44 @@ var key;
 var enabled;
 var library;
 
-
 function extractHostname(url) {
-    var hostname;
-    if (url.indexOf("://") > -1) {
-        hostname = url.split('/')[2];
-    } else {
-        hostname = url.split('/')[0];
+    try {
+        const { hostname } = new URL(url);
+        return hostname;
     }
-    hostname = hostname.split(':')[0];
-    hostname = hostname.split('?')[0];
-    return hostname;
+    catch(_) {
+        return undefined;
+    }
 }
 
 function saveChanges(dont_run) {
     var _code = editor.getValue();
-    var js = {'code':_code, 'enabled':enabled, 'library':library};
+    var js = { 'code': _code, 'enabled': enabled, 'library': library };
 
-    if (_code == undefined || _code == 'undefined') {
-        js = {'code':'','enabled':'true', 'library':'jquery_3_3_1'};
+    if(_code == undefined || _code == 'undefined') {
+        js = { 'code': '', 'enabled': 'true', 'library': 'jquery_3_3_1' };
     }
 
     //set the library again...
     library = document.getElementById('slLibrary').value;
 
     var _data = new Object();
-    _data['runjavascript_'+host] = js;
+    _data['runjavascript_' + host] = js;
 
-    chrome.storage.sync.set(_data, function() {});
+    chrome.storage.sync.set(_data, function() { });
 
-    if (typeof dont_run === 'undefined') {
+    if(typeof dont_run === 'undefined') {
         //stop toggleOnHost from running the script again!
     }
     else {
-        if (enabled) {
+        if(enabled) {
             chrome.tabs.executeScript(null, {
                 code: js.code
             });
         }
     }
     document.getElementById("lblUpdated").style.display = "inline-block";
+
     setTimeout(function() {
         document.getElementById("lblUpdated").style.display = "none";
     }, 2000);
@@ -68,18 +66,19 @@ function setLibrary() {
 function update_ace_placeholder() {
     var shouldShow = !editor.session.getValue().length;
     var node = editor.renderer.emptyMessageNode;
-
     var node2 = editor.renderer.emptyMessageNode2;
-    if (!shouldShow && node) {
+
+    if(!shouldShow && node) {
         editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode);
         editor.renderer.emptyMessageNode = null;
 
         editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode2);
         editor.renderer.emptyMessageNode2 = null;
-        
+
         editor.renderer.scroller.removeChild(editor.renderer.emptyMessageNode3);
         editor.renderer.emptyMessageNode3 = null;
-    } else if (shouldShow && !node) {
+    }
+    else if(shouldShow && !node) {
         node = editor.renderer.emptyMessageNode = document.createElement("div");
         node.textContent = "Add your Javascript here and click 'Save and Run'"
         node.className = "ace_invisible ace_emptyMessage"
@@ -104,29 +103,42 @@ chrome.tabs.query({
     active: true,
     lastFocusedWindow: true
 }, function(tabs) {
+    console.log('popup: on chrome.tabs.query callback');
+
     var tab = tabs[0];
-    if (tab) {
+    if(tab) {
         host = extractHostname(tab.url);
-        key = 'runjavascript_'+host;
+        key = `runjavascript_${host}`;
+
         document.getElementById('_toggle_host_name').innerHTML = host;
-        //setAndRenderEnabledOn(host);
         showRightToggleOnHostButton();
     }
 });
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('popup: on DOMContentLoaded');
+
     editor = ace.edit("editor");
     document.getElementById('runJavascript').addEventListener('click', saveChanges);
     document.getElementById('chkToggleOnHost').addEventListener('change', toggleOnHost);
     document.getElementById('slLibrary').addEventListener('change', setLibrary);
 
+
+
+    editor.on("input", update_ace_placeholder);
+    setTimeout(update_ace_placeholder, 100);
+});
+
+chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.get(function(obj) {
-        var js = obj['runjavascript_'+host];
-        if (typeof js == 'string') {
-            js = {'code':js,'enabled':'true','library':''};
+        console.log('popup: on chrome.storage.sync.get');
+
+        var js = obj['runjavascript_' + host];
+        if(typeof js == 'string') {
+            js = { 'code': js, 'enabled': 'true', 'library': '' };
         }
-        if (typeof js == undefined) {
-            js = {'code':'','enabled':'true','library':''};
+        if(typeof js == undefined) {
+            js = { 'code': '', 'enabled': 'true', 'library': '' };
         }
         enabled = js && js.enabled ? js.enabled : false;
         showRightToggleOnHostButton();
@@ -134,48 +146,4 @@ document.addEventListener('DOMContentLoaded', function () {
         library = js && js.library ? js.library : "";
         document.getElementById('slLibrary').value = library;
     });
-
-    editor.on("input", update_ace_placeholder);
-    setTimeout(update_ace_placeholder, 100);
-
 });
-
-// Standard Google Universal Analytics code
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
-    ga('create', 'UA-68468918-2', 'auto');
-    ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
-    ga('require', 'displayfeatures');
-    ga('send', 'pageview', '/popup.html');
-    
-
-function trackButton(e) {
-    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-};
-
-var buttons = document.querySelectorAll('button');
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', trackButtonClick);
-}
-var anchors = document.querySelectorAll('a');
-for (var i = 0; i < anchors.length; i++) {
-    anchors[i].addEventListener('click', trackButtonClick);
-}
-var inputs = document.querySelectorAll('input');
-for (var i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener('click', trackButtonClick);
-}
-
-// function getEnabledOn() {
-//     chrome.storage.sync.get(function(obj) {
-
-//     });
-// }
-
-// function setAndRenderEnabledOn(host) {
-//     console.log(host);
-//     document.getElementById('regexMatchHost').value = host+'/*';
-//     document.getElementById('regexMatchHost').placeholder = host+'/*';
-// }
